2beede45c645158615b28dd48e28327335f3494f
박겸영 <yong_drg@naver.com>
2022-07-15 22:54:32 +0900

    feat: 가로 스크롤 뷰

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    'prettier/react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>29º</Text>
+          <Text style={styles.description}>일요일</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>29º</Text>
+          <Text style={styles.description}>일요일</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>29º</Text>
+          <Text style={styles.description}>일요일</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>29º</Text>
+          <Text style={styles.description}>일요일</Text>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
+  },
+  city: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 20,
+  },
+  cityName: {
+    fontSize: 48,
+    fontWeight: '600',
+  },
+  weather: {
+    backgroundColor: 'lavender',
+  },
+  day: {
+    alignItems: 'center',
+    width: SCREEN_WIDTH,
+  },
+  temp: {
+    fontSize: 120,
+  },
+  description: {
+    marginTop: 10,
+    fontSize: 36,
   },
 });
